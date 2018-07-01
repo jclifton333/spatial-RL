438a02bd07bb8fe2a9c08413c51d5f1600728431
Jesse Clifton <jclifton333@gmail.com>
2018-07-01 11:42:24 -0500

    start on qp

--- src/estimation/optim/qp/qp_max.py
+++ src/estimation/optim/qp/qp_max.py
@@ -1,33 +1,58 @@
 """
 Solve a quadratic binary program with treatment_budget constraint.
 For taking argmax of quadratic approximation to Q function.
+
+
+ref: http://www.gurobi.com/documentation/7.0/examples/dense_py.html
 """
-import gurobipy as gb
 import numpy as np
+from gurobipy import *
 
 
-def qp_max(M, b, r, constraint):
+def qp_max(M, b, r, budget):
   """
   max x'Mx + x'b + r
-  s.t. 1'x = constraint
+  s.t. 1'x = budget
 
   :param M:
   :param b:
   :param r:
-  :param constraint:
+  :param budget:
   :return:
   """
 
-  m = gb.Model('qip')
+  model = Model('qip')
   L = len(b)
-  # ToDo: add variables
+
+  # Define decision variables
+  vars = []
+  for j in range(L):
+    vars.append(model.addVar(vtype=GRB.BINARY))
 
   # Define objective
-  obj = np.dot(np.dot(M, x), x) + np.dot(b, x) + r
-  m.setObjective(obj)
+  obj = QuadExpr()
+  for i in range(L):
+    for j in range(L):
+      obj += M[i,j]*vars[i]*vars[j]
+  for i in range(L):
+    obj += b[i]*vars[i]
+  obj += r
+  model.setObjective(obj)
 
   # Define constraint
-  m.addConstr(np.dot(np.ones(b), x) == constraint)
+  constr_expr = LinExpr()
+  constr_expr.addTerms(1.0, vars)
+  model.addConstr(constr_expr == budget)
 
   # Optimize
-  m.optimize()
+  model.optimize()
+
+  return
+
+
+if __name__ == '__main__':
+  M = np.eye(2)
+  b = np.ones(2)
+  r = 0
+  budget = 1
+  qp_max(M, b, r, budget)
