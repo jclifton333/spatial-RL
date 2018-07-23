d41a13589f91113eedd1dd15612e4aa86bea6229
Jesse Clifton <jclifto@laber-gpu01.stat.ncsu.edu>
2018-07-23 18:17:57 -0400

    misc

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=1 --rollout_depth=0 --time_horizon=25 --L=100 --gamma=0.9 
+     --omega=0.0 --number_of_replicates=5 --rollout_depth=0 --time_horizon=25 --L=100 --gamma=0.9 &
 
-# python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based' --argmaxer_name='quad_approx' \
-#      --omega=1.0 --number_of_replicates=5 --rollout_depth=0 --time_horizon=25 --L=100 --gamma=0.9 
+python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based' --argmaxer_name='quad_approx' \
+     --omega=1.0 --number_of_replicates=5 --rollout_depth=0 --time_horizon=25 --L=100 --gamma=0.9 
 
