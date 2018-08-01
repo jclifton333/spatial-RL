c2ad9e726495b8609bd6b95ac626c1305208061e
Jesse Clifton <jclifto@laber-gpu01.stat.ncsu.edu>
2018-07-31 21:04:27 -0400

    sklearn time

--- bin/run.sh
+++ bin/run.sh
@@ -10,3 +10,15 @@ python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_n
 python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based_one_step' --argmaxer_name='quad_approx' \
      --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=0.5
+
+python3 ../src/run/run.py --env_name='SIS' --policy_name='random' --argmaxer_name='quad_approx' \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --evaluation_budget=100 --epsilon=1.0
+
+python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_name='quad_approx' \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --evaluation_budget=100 --epsilon=1.0
+
+python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based_one_step' --argmaxer_name='quad_approx' \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --evaluation_budget=100 --epsilon=1.0
