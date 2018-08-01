7e9c26fdb57695625541b5b35b74452038f17246
Jesse Clifton <jclifto@laber-gpu01.stat.ncsu.edu>
2018-07-31 21:13:19 -0400

    will more data help? prob not

--- bin/run.sh
+++ bin/run.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 python3 ../src/run/run.py --env_name='SIS' --policy_name='random' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
-     --evaluation_budget=100 --epsilon=0.5
-
-python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
-     --evaluation_budget=100 --epsilon=0.5
-
-python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based_one_step' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
-     --evaluation_budget=100 --epsilon=0.5
-
-python3 ../src/run/run.py --env_name='SIS' --policy_name='random' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=50 --L=100 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=1.0
 
 python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=50 --L=100 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=1.0
 
 python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based_one_step' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=50 --L=100 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=1.0
