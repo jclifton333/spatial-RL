40c35b5e534e0577fcd79899ebef5ded7a1a5e4a
Jesse Clifton <jclifto@laber-gpu01.stat.ncsu.edu>
2018-07-31 20:05:40 -0400

    rando result

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python3 ../src/run/run.py --env_name='SIS' --policy_name='SIS_model_based_one_step' --argmaxer_name='quad_approx' \
+python3 ../src/run/run.py --env_name='SIS' --policy_name='random' --argmaxer_name='quad_approx' \
      --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
-     --evaluation_budget=100 --epsilon=0.5 &
+     --evaluation_budget=100 --epsilon=0.5 
 
-python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_name='quad_approx' \
-     --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
-     --evaluation_budget=100 --epsilon=0.5
+# python3 ../src/run/run.py --env_name='SIS' --policy_name='one_step' --argmaxer_name='quad_approx' \
+#      --omega=0.0 --number_of_replicates=40 --rollout_depth=1 --time_horizon=25 --L=50 --gamma=0.9 \
+#      --evaluation_budget=100 --epsilon=0.5
