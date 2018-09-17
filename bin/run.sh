41177135db9350e637ff011e3cf2dbeee546c2d3
Jesse Clifton <jclifto@laber-gpu02.stat.ncsu.edu>
2018-09-17 16:28:57 -0400

    misc

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 ../src/run/run.py --env_name='Ebola' --policy_name='one_step_mse_averaged' --argmaxer_name='quad_approx' \
+python3 ../src/run/run.py --env_name='Ebola' --policy_name='one_step' --argmaxer_name='quad_approx' \
      --omega=0.0 --number_of_replicates=1 --rollout_depth=1 --time_horizon=25 --L=100 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=0.0
 
