a7dff1146cf1e95e71d0d9d73dac3a42676a6660
Jesse Clifton <jclifton333@gmail.com>
2019-03-27 17:57:42 -0400

    ready to run on cluster

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python3 ../src/run/run.py --env_name='sis' --policy_name='two_step' --argmaxer_name='random' \
-     --omega=0.0 --number_of_replicates=1 --rollout_depth=1 --time_horizon=25 --L=30 --gamma=0.9 \
+python3 ../src/run/run.py --env_name='sis' --policy_name='two_step' --argmaxer_name='quad_approx' \
+     --omega=0.0 --number_of_replicates=48 --rollout_depth=1 --time_horizon=50 --L=100 --gamma=0.9 \
      --evaluation_budget=100 --epsilon=0.0 --network='lattice' --seed=1
 
 
