201819efabc5fec84d1f2da399ad9a4fb8c925fe
Jesse Clifton <jclifton333@gmail.com>
2019-04-15 18:12:33 -0400

    report epsilon

--- analysis/results/summarize_results.py
+++ analysis/results/summarize_results.py
@@ -4,6 +4,7 @@ import argparse
 
 
 def summarize_results_at_date(date_str):
+  epsilons = ['0.0', '0.5', '0.75', '1.0']
   any_found = False
   for fname in os.listdir():
     if date_str in fname:
@@ -11,7 +12,10 @@ def summarize_results_at_date(date_str):
       f = yaml.load(open(fname, 'rb'))
       mean_, se_ = f['results']['mean'], f['results']['se']
       env_name, L, policy_name = f['settings']['env_name'], f['settings']['L'], f['settings']['policy_name']
-      print(env_name, policy_name, L, mean_, se_)
+      for eps in epsilons:
+        if eps in fname:
+          epsilon = eps
+      print(env_name, policy_name, L, epsilon, mean_, se_)
   if not any_found:
     print('No results found for that date.')
 
