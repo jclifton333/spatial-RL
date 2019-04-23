4705f0eaaaff9d8661fa2e265e1a1e5d3e22b708
jesse <jclifton333@gmail.com>
2019-04-23 16:28:51 -0400

    continuing to mplement dyna

--- src/policies/dyna_policies.py
+++ src/policies/dyna_policies.py
@@ -1,5 +1,31 @@
+import os
+this_dir = os.path.dirname(os.path.abspath(__file__))
+
+from src.estimation.q_functions.fqi import fqi
+from src.estimation.q_functions.regressor import AutoRegressor
+from src.estimation.q_functions.q_functions import q, q_max_all_states
+from src.estimation.model_based.sis.estimate_sis_q_fn import estimate_sis_q_fn
+from src.estimation.model_based.sis.estimate_sis_parameters import fit_sis_transition_model
+from src.estimation.model_based.Gravity.estimate_ebola_parameters import fit_ebola_transition_model
+from src.estimation.model_based.Gravity.estimate_continuous_parameters import fit_continuous_grav_transition_model
+from src.estimation.q_functions.model_fitters import SKLogit2
+import src.estimation.q_functions.mse_optimal_combination as mse_combo
+from src.estimation.q_functions.one_step import *
+from src.utils.misc import random_argsort
+from sklearn.ensemble import RandomForestRegressor, IsolationForest
+from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge
+from scipy.special import expit, logit
+import numpy as np
+import keras.backend as K
+from functools import partial
+
+
 def sis_one_step_dyna(**kwargs):
-  env = kwargs['env'] 
+  env, treatment_budget, evaluation_budget, argmaxer, gamma, regressor = \
+    kwargs['env'], kwargs['treatment_budget'], kwargs['evaluation_budget'], kwargs['argmaxer'], \
+    kwargs['gamma'], kwargs['regressor']
+  q_mb_one_step, _ = fit_one_step_sis_mb_q(env)
+
   MAX_NUM_NONZERO = np.min((env.max_num_neighbors, 8))
   QUOTA = int(np.sqrt(env.L * env.T))
 
@@ -10,13 +36,13 @@ def sis_one_step_dyna(**kwargs):
   # ToDo: this shouldn't be done each time the policy is called...
   unique_feature_indicators = {}
   for n in range(MAX_NUM_NONZERO):
-    truth_vals = [1 for i in range(n)[ + [0 for i in range(8 - n)]      
+    truth_vals = [1 for i in range(n)] + [0 for i in range(8 - n)]
     for permutation_ in all_permutations(truth_vals):
       for raw_feature_combo in raw_feature_combos:
         feature_combo = tuple(raw_feature_combo + permutation_)
         unique_feature_indicators[feature_combo] = {'count': 0, 'list': []}
 
-  mb_phats = [mb_clf(x) for x in env.X]
+  mb_phats = [q_mb_one_step(x) for x in env.X]
 
   # Count number of feature indicators
   for t, X_ in enumerate(env.X):
@@ -26,41 +52,35 @@ def sis_one_step_dyna(**kwargs):
       unique_feature_indicators[tuple(x_indicator)]['list'].append((x, t, l)) 
 
   # Supplement features that fall short of quota
-  X_synthetic = np.zeros((0, X_indicator.shape[1])) 
+  X_synthetic = np.zeros((0, env.X[0].shape[1]))
   Y_synthetic = np.zeros(0)
   for feature_info in unique_feature_indicators.values():
-  count = feature_info['count']  # ToDo: what if count=0?
+    count = feature_info['count']  # ToDo: what if count=0?
     if count < QUOTA:
-      num_fake_data = QUOTA - count 
+      num_fake_data = QUOTA - count
 
       # Sample with replacement up to desired number
       feature_list = feature_info['list']
       synthetic = np.random.choice(feature_list, num_fake_data, replace=T)
-      x_synthetic = [o_[0] for o_ in feature_list]
-      y_synthetic = [mb_phats[o_[1]][o_[2]] for o_ in feature_list]
+      x_synthetic = [o_[0] for o_ in synthetic]
+      y_synthetic = [mb_phats[o_[1]][o_[2]] for o_ in synthetic]
 
       # Add to dataset
       X_synthetic = np.vstack((X_synthetic, x_synthetic))
-      y_synthetic = np.hstack((Y_synthetic, y_synthetic))
+      Y_synthetic = np.hstack((Y_synthetic, y_synthetic))
 
-   # Fit model-free model on new dataset
-   X_new = np.vstack((np.vstack(env.X), X_synthetic)) 
-   y_new = np.hstack((env.y, y_synthetic))
+  # Fit model-free model on new dataset
+  X_new = np.vstack((np.vstack(env.X), X_synthetic))
+  y_new = np.hstack((env.y, Y_synthetic))
+  infected_indices = None
+  q0 = SKLogit2()
+  q0.fit(X_new, y_new, infected_indices, None)
 
+  # Define q-function
+  def qfn(a):
+    x = env.data_block_at_action(-1, a)
+    infected_indices = np.where(env.Y[-1, :] == 1)
+    return q0.predict_proba(x, infected_indices, None)
 
-      
-    
-
-
-   
-
-   
-    
-
-  
-
-      
-
-  
-  
-
+  a_ = argmaxer(qfn, evaluation_budget, treatment_budget, env)
+  return a_, {}
