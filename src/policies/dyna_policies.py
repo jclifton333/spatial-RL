005b1434a5ac747abfe38d8be383236d8e8b84bb
jesse <jclifton333@gmail.com>
2019-04-23 22:55:11 -0400

    playing with dyna

--- src/policies/dyna_policies.py
+++ src/policies/dyna_policies.py
@@ -71,36 +71,39 @@ def sis_one_step_dyna(**kwargs):
   # Supplement features that fall short of quota
   X_synthetic = np.zeros((0, env.X[0].shape[1]))
   Y_synthetic = np.zeros(0)
-  for feature_info in unique_feature_indicators.values():
-    count = feature_info['count']
-    if 0 < count < QUOTA:  # ToDo: what if count=0?
-      num_fake_data = QUOTA - count
+  # for feature_info in unique_feature_indicators.values():
+  #   count = feature_info['count']
+  #   if 0 < count < QUOTA:  # ToDo: what if count=0?
+  #     num_fake_data = QUOTA - count
 
-      # Sample with replacement up to desired number
-      feature_list = feature_info['list']
-      synthetic_ixs = np.random.choice(len(feature_list), num_fake_data, replace=True)
-      synthetic = [feature_list[ix] for ix in synthetic_ixs]
-      x_synthetic = [o_[0] for o_ in synthetic]
-      p_synthetic = [mb_phats[o_[1]][o_[2]] for o_ in synthetic]
-      y_synthetic = np.random.binomial(1, p_synthetic)
+  #     # Sample with replacement up to desired number
+  #     feature_list = feature_info['list']
+  #     synthetic_ixs = np.random.choice(len(feature_list), num_fake_data, replace=True)
+  #     synthetic = [feature_list[ix] for ix in synthetic_ixs]
+  #     x_synthetic = [o_[0] for o_ in synthetic]
+  #     y_synthetic = [mb_phats[o_[1]][o_[2]] for o_ in synthetic]
+  #     # y_synthetic = np.random.binomial(1, p_synthetic)
 
-      # Add to dataset
-      X_synthetic = np.vstack((X_synthetic, x_synthetic))
-      Y_synthetic = np.hstack((Y_synthetic, y_synthetic))
+  #     # Add to dataset
+  #     X_synthetic = np.vstack((X_synthetic, x_synthetic))
+  #     Y_synthetic = np.hstack((Y_synthetic, y_synthetic))
 
-  # Fit model-free model on new dataset
+  # # Fit model-free model on new dataset
   X_new = np.vstack((np.vstack(env.X), X_synthetic))
   y_new = np.hstack((np.hstack(env.y), Y_synthetic))
-  infected_indices = np.where(convert_first_order_to_infection_status(X_new) == 1)
+  # infected_indices = np.where(convert_first_order_to_infection_status(X_new) == 1)
 
-  q0 = SKLogit2()
-  q0.fit(X_new, y_new, None, False, infected_indices, None)
+  # q0 = SKLogit2()
+  # q0.fit(X_new, y_new, None, False, infected_indices, None)
+  q0 = Ridge()
+  q0.fit(X_new, y_new)
 
   # Define q-function
   def qfn(a):
     x = env.data_block_at_action(-1, a)
-    infected_indices = np.where(env.Y[-1, :] == 1)[0]
-    return q0.predict_proba(x, infected_indices, None)
+    # infected_indices = np.where(env.Y[-1, :] == 1)[0]
+    # return q0.predict_proba(x, infected_indices, None)
+    return q0.predict(x)
 
   a_ = argmaxer(qfn, evaluation_budget, treatment_budget, env)
   return a_, {}
