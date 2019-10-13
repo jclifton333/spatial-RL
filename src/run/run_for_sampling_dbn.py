80c733500559daa40bfca1f6e2493dd7acfbf571
jesse <jclifton333@gmail.com>
2019-10-13 18:17:28 -0400

    sampling dbn run

--- src/run/run_for_sampling_dbn.py
+++ src/run/run_for_sampling_dbn.py
@@ -13,29 +13,14 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 pkg_dir = os.path.join(this_dir, '..', '..')
 sys.path.append(pkg_dir)
 
-from src.environments import generate_network
-from src.policies.prefit_policies import generate_two_step_sis_data
 from src.run.Simulator import Simulator
 
-VALID_ENVIRONMENT_NAMES = ['sis', 'Ebola', 'ContinuousGrav']
-VALID_POLICY_NAMES = ['random', 'no_action', 'true_probs', 'true_probs_myopic', 'fqi', 'one_step', 'two_step',
-                      'treat_all', 'sis_model_based_one_step', 'two_step_mb',
-                      'one_step_mse_averaged', 'sis_two_step_mse_averaged',
-                      'gravity_model_based_one_step', 'gravity_model_based_myopic', 'policy_search',
-                      'sis_one_step_equal_averaged', 'one_step_stacked', 'sis_model_based_myopic',
-                      'two_step_higher_order', 'two_step_sis_prefit', 'one_step_truth_augmented',
-                      'one_step_projection_combo', 'two_step_stacked', 'sis_aic_two_step', 'sis_aic_one_step',
-                      'sis_one_step_continuation', 'ebola_aic_one_step', 'sis_one_step_dyna_space_filling',
-                      'sis_local_aic_one_step', 'ebola_aic_two_step']
-POLICY_SEARCH_NAMES = ['policy_search', 'sis_aic_two_step', 'sis_one_step_continuation', 'ebola_aic_two_step']
-VALID_ARGMAXER_NAMES = ['quad_approx', 'random', 'global', 'sequential_quad_approx']
-VALID_NETWORK_NAMES = ['lattice', 'barabasi', 'nearestneighbor']
+POLICY_NAME = 'random'
+ENV_NAME = 'sis'
+NETWORK = 'lattice'
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('--env_name', type=str, choices=VALID_ENVIRONMENT_NAMES)
-  parser.add_argument('--policy_name', type=str, choices=VALID_POLICY_NAMES)
-  parser.add_argument('--argmaxer_name', type=str, choices=VALID_ARGMAXER_NAMES)
   parser.add_argument('--number_of_replicates', type=int)
   parser.add_argument('--omega', type=float)
   parser.add_argument('--rollout_depth', type=int)
@@ -44,7 +29,6 @@ if __name__ == '__main__':
   parser.add_argument('--gamma', type=float)
   parser.add_argument('--evaluation_budget', type=int)
   parser.add_argument('--epsilon', type=float)
-  parser.add_argument('--network', type=str, choices=VALID_NETWORK_NAMES)
   parser.add_argument('--ts', type=str, choices=['True', 'False'])
   parser.add_argument('--seed', type=int)
   parser.add_argument('--num_prefit_data', type=float)
@@ -52,36 +36,14 @@ if __name__ == '__main__':
   parser.add_argument('--ignore_errors', type=str)
   args = parser.parse_args()
 
-  network_dict = {'lattice': generate_network.lattice, 'barabasi': generate_network.Barabasi_Albert,
-                  'nearestneighbor': generate_network.random_nearest_neighbor}
+  env_kwargs = {'L': args.L, 'omega': args.omega, 'generate_network': NETWORK,
+                'initial_infections': None, 'add_neighbor_sums': False, 'epsilon': args.epsilon}
 
-  if args.env_name == 'sis':
-    env_kwargs = {'L': args.L, 'omega': args.omega, 'generate_network': network_dict[args.network],
-                  'initial_infections': None, 'add_neighbor_sums': False, 'epsilon': args.epsilon}
-    if args.network == 'nearestneighbor':
-      env_kwargs['regenerate_network'] = True
-
-    network_name = args.network
-  elif args.env_name == 'Ebola':
-    env_kwargs = {}
-    network_name = 'Ebola'
-  elif args.env_name == 'ContinuousGrav':
-    env_kwargs = {'L': args.L}
-    network_name = 'ContinuousGrav'
   ts = (args.ts == 'True')
   ignore_errors = (args.ignore_errors == 'True')
-  if args.policy_name in POLICY_SEARCH_NAMES:
-    env_kwargs['construct_features_for_policy_search'] = True
-    if 'sis' in args.policy_name:
-      env_kwargs['neighbor_features'] = False
 
-  # Generate data if using a prefit policy
-  # ToDo: Currently only implemented for two-step mf sis
-  if 'prefit' in args.policy_name:
-    generate_two_step_sis_data(args.L, args.time_horizon, args.network, args.num_prefit_data)
-
-  Sim = Simulator(args.rollout_depth, args.env_name, args.time_horizon, args.number_of_replicates, args.policy_name,
-                  args.argmaxer_name, args.gamma, args.evaluation_budget, env_kwargs, network_name, ts, args.seed,
+  Sim = Simulator(args.rollout_depth, ENV_NAME, args.time_horizon, args.number_of_replicates, POLICY_NAME,
+                  'random', args.gamma, args.evaluation_budget, env_kwargs, NETWORK, ts, args.seed,
                   args.error_quantile, fit_qfn_at_end=True, sampling_dbn_run=True, ignore_errors=ignore_errors)
   if args.number_of_replicates == 1:
     Sim.episode(0)
