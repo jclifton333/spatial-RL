016bf89dec1ca7386be463683a7c6039690fa500
Jesse Clifton <jclifto@laber-gpu02.stat.ncsu.edu>
2019-11-05 10:52:13 -0500

    change analysis

--- analysis/analyze_sampling_dbns.py
+++ analysis/analyze_sampling_dbns.py
@@ -16,7 +16,8 @@ def summarize_sampling_dbns(fname_list, save=False):
   # 20: (~s, ~a, y)
   # which are redundant with features in the main effect component
 
-  results_dict = {'L': [], 'policy': [], 'network': [], 'coverages': [], 'bootstrap_pvals': []}
+  results_dict = {'L': [], 'policy': [], 'network': [], 'min_coverages': [], 'median_coverages': [], 
+                  'max_coverages': []}
   for fname in fname_list:
     d = yaml.load(open('./results/{}'.format(fname), 'rb'))
 
@@ -28,20 +29,28 @@ def summarize_sampling_dbns(fname_list, save=False):
     mean_bootstrap_pvals = [mean_bootstrap_pvals[ix] for ix in range(len(mean_bootstrap_pvals)) if ix in INDICES_TO_KEEP] 
 
     # Get gen model settings
+    # ToDo: store these in results so we dont have to do this hacky shit
     L = d['settings']['L']
     policy = (len(d['results']['coverages']) == 33)
     for name in NETWORK_NAMES:
       if name in fname:
         network_name = name
+    if 'true_probs_myopic' in fname:
+      policy_name = 'true_probs_myopic'
+    else:
+      policy_name = 'random'
 
     # Add to results
     results_dict['L'].append(L)
-    results_dict['policy'].append(policy)
+    results_dict['policy'].append(policy_name)
     results_dict['network'].append(network_name)
-    results_dict['coverages'].append(np.min(coverages))
-    results_dict['bootstrap_pvals'].append(mean_bootstrap_pvals)
+    results_dict['min_coverages'].append(np.min(coverages))
+    results_dict['median_coverages'].append(np.median(coverages))
+    results_dict['max_coverages'].append(np.max(coverages))
+    # results_dict['bootstrap_pvals'].append(mean_bootstrap_pvals)
 
   df = pd.DataFrame(results_dict)
+  df.sort_values(by=['network', 'policy', 'L'], inplace=True)
   pdb.set_trace()
 
   if save: 
@@ -59,19 +68,25 @@ if __name__ == "__main__":
                  'sis_true_probs_myopic_random_100_lattice_sampling-dbn-run=True_0.0_191104_110012.yml',
                  'sis_true_probs_myopic_random_25_lattice_sampling-dbn-run=True_0.0_191104_105124.yml',
                  'sis_true_probs_myopic_random_300_lattice_sampling-dbn-run=True_0.0_191104_111133.yml',
-                 'sis_true_probs_myopic_random_50_lattice_sampling-dbn-run=True_0.0_191104_105510.yml'
-                 ]
-  fname_list_1_step = ['sis_random_random_100_nearestneighbor_sampling-dbn-run=True_0.0_191104_152354.yml',
-                 'sis_random_random_25_nearestneighbor_sampling-dbn-run=True_0.0_191104_151444.yml',
+                 'sis_true_probs_myopic_random_50_lattice_sampling-dbn-run=True_0.0_191104_105510.yml',
+                 'sis_random_random_100_nearestneighbor_sampling-dbn-run=True_0.0_191104_152354.yml', 
+                 'sis_random_random_25_nearestneighbor_sampling-dbn-run=True_0.0_191104_151444.yml', 
+                 'sis_random_random_25_lattice_sampling-dbn-run=True_0.0_191104_145243.yml', 
+                 'sis_random_random_300_lattice_sampling-dbn-run=True_0.0_191104_151227.yml', 
                  'sis_random_random_300_nearestneighbor_sampling-dbn-run=True_0.0_191104_153606.yml',
-                 'sis_random_random_50_nearestneighbor_sampling-dbn-run=True_0.0_191104_151837.yml',
-                 'sis_true_probs_myopic_random_50_lattice_sampling-dbn-run=True_0.0_191104_170925.yml',
+                 'sis_random_random_50_lattice_sampling-dbn-run=True_0.0_191104_145633.yml', 
+                 'sis_random_random_50_nearestneighbor_sampling-dbn-run=True_0.0_191104_151837.yml', 
+                 'sis_random_random_100_lattice_sampling-dbn-run=True_0.0_191104_150118.yml'
+                 ]
+
+  # ToDo: double check that these are all 1-step (not 0-step)
+  fname_list_1_step = ['sis_true_probs_myopic_random_50_lattice_sampling-dbn-run=True_0.0_191104_170925.yml',
                  'sis_true_probs_myopic_random_300_lattice_sampling-dbn-run=True_0.0_191104_174921.yml',
                  'sis_true_probs_myopic_random_25_lattice_sampling-dbn-run=True_0.0_191104_170226.yml',
                  'sis_true_probs_myopic_random_100_lattice_sampling-dbn-run=True_0.0_191104_172008.yml'
                  ]
 
-  summarize_sampling_dbns(fname_list_0_step, save=True)
+  summarize_sampling_dbns(fname_list_1_step, save=False)
 
 
 
--- bin/run_for_sampling_dbn.sh
+++ bin/run_for_sampling_dbn.sh
@@ -1,51 +1,35 @@
 #!/usr/bin/env bash
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=25 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='true_probs_myopic'
+      --L=500 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='lattice' --policy='true_probs_myopic' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=50 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='true_probs_myopic'
+      --L=1000 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='lattice' --policy='true_probs_myopic' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=100 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='true_probs_myopic'
+      --L=500 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='lattice' --policy='random' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=300 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='true_probs_myopic'
+      --L=1000 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='lattice' --policy='random' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=25 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='random'
+      --L=500 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='nearestneighbor' --policy='random' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=50 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='random'
+      --L=1000 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='nearestneighbor' --policy='random' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=100 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='random'
+      --L=500 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='nearestneighbor' --policy='true_probs_myopic' --sampling_dbn_estimator='two_step'
 
 python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=300 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='lattice' --policy='random'
-
-python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=25 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='nearestneighbor' --policy='random'
-
-python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=50 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='nearestneighbor' --policy='random'
-
-python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=100 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='nearestneighbor' --policy='random'
-
-python3 ../src/run/run_for_sampling_dbn.py  --omega=0.0 --number_of_replicates=480 --rollout_depth=1 --time_horizon=25 \
-      --L=300 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
-      --network='nearestneighbor' --policy='random'
+      --L=1000 --gamma=0.9 --evaluation_budget=100 --epsilon=0.0 --seed=7 --error_quantile=0.95 --ignore_errors='True' \
+      --network='nearestneighbor' --policy='true_probs_myopic' --sampling_dbn_estimator='two_step'
 
 
 
