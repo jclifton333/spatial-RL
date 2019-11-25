92ee388b16855e588b8511aab230c6f760b289ff
Jesse Clifton <jclifto@laber-gpu01.stat.ncsu.edu>
2019-11-24 21:57:51 -0500

    add constant cutoff to list of valid

--- src/run/run_for_sampling_dbn.py
+++ src/run/run_for_sampling_dbn.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
   parser.add_argument('--policy', type=str)
   parser.add_argument('--sampling_dbn_estimator', type=str, choices=['one_step', 'two_step', 'two_step_random',
                                                                      'two_step_mb_myopic',
-                                                                     'two_step_constant_cutoff'])
+                                                                     'two_step_mb_constant_cutoff'])
   args = parser.parse_args()
 
   network_dict = {'lattice': generate_network.lattice, 'barabasi': generate_network.Barabasi_Albert,
