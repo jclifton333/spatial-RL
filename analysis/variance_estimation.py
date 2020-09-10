8ef33c0c3e0a9b65f50902f95eb33592673d8a3e
Jesse Clifton <jesseclifton@Jesses-iMac-Pro.local>
2020-09-10 21:47:22 +0100

    parallelize

--- analysis/variance_estimation.py
+++ analysis/variance_estimation.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.linalg as la
 import pdb
+import multiprocessing as mp
+from functools import partial
 
 
 def get_exponential_gaussian_covariance(beta1=1, beta2=2, grid_size=100):
@@ -73,21 +75,42 @@ def get_sigma_sq_infty(observations):
   num_rep, L = observations.shape
   x_bar = observations.mean(axis=0)
   x_0 = observations[:, 0] - x_bar[0]
+  x_0_sq = x_0**2
+  x_0_sq_mean = np.mean(x_0_sq)
+  x_0_sq_centered = x_0_sq - x_0_sq_mean
   sigma_sq_infty = 0.
+  sq_residual_cov = 0.
 
   for l in range(L):
     x_l = observations[:, l] - x_bar[l]
+    x_l_sq = x_l**2
+    x_l_sq_mean = np.mean(x_l_sq)
+    x_l_sq_centered = x_l_sq - x_l_sq_mean
     cov_0l = np.dot(x_0, x_l) / num_rep
+    cov_sq_0l = np.dot(x_0_sq_centered, x_l_sq_centered) / num_rep
     sigma_sq_infty += cov_0l
+    sq_residual_cov += cov_sq_0l
 
-  return sigma_sq_infty
+  return sigma_sq_infty, sq_residual_cov
 
 
-def get_var_estimate_mse(kernel, root_cov, pairwise_distances, n_rep=10000):
+def draw_from_gaussian_and_estimate_var(ix, root_cov, kernel_weights, grid_size):
+  y = generate_gaussian(root_cov)
+
+  # Products of centered residuals
+  y_centered = y - np.mean(y)
+  cross_raw = np.outer(y_centered, y_centered)
+
+  cross_kernel = np.multiply(cross_raw, kernel_weights)
+  sigma_sq_hat = cross_kernel.sum() / grid_size
+
+  return y, sigma_sq_hat
+
+
+def get_var_estimate_mse(kernel, root_cov, pairwise_distances, n_rep=10000, pct_cores=0.25):
   var_estimates = np.zeros(0)
-  observations = np.zeros((0, grid_size))
   grid_size = root_cov.shape[0]
-
+  observations = np.zeros((0, grid_size))
 
   # Construct kernel weight matrix
   kernel_weights = np.zeros((grid_size, grid_size))
@@ -95,22 +118,24 @@ def get_var_estimate_mse(kernel, root_cov, pairwise_distances, n_rep=10000):
     for j in range(grid_size):
       kernel_weights[i, j] = kernel(pairwise_distances[i, j])
 
-  for _ in range(n_rep):
-    y = generate_gaussian(root_cov)
+  replicate = partial(draw_from_gaussian_and_estimate_var, root_cov=root_cov, kernel_weights=kernel_weights,
+                      grid_size=grid_size)
 
-    # Products of centered residuals
-    y_centered = y - np.mean(y)
-    cross_raw = np.outer(y_centered, y_centered)
+  # Parallelize calculations on draws
+  n_cpu = mp.cpu_count()
+  n_processes = np.ceil(pct_cores*n_cpu).astype(int)
+  pool = mp.Pool(processes=n_processes)
+  res_list = pool.map(replicate, range(n_rep))
 
-    cross_kernel = np.multiply(cross_raw, kernel_weights)
-    sigma_sq_hat = cross_kernel.sum() / grid_size
+  # Collect results
+  for y, sigma_sq_hat in res_list:
+    observations = np.vstack((observations, y))
     var_estimates = np.hstack((var_estimates, sigma_sq_hat))
 
-    observations = np.vstack((observations, y))
-
   # autocovs = get_autocov_sq_sequence(observations, pairwise_distances)
-  sigma_sq_infty = get_sigma_sq_infty(observations)
+  sigma_sq_infty, sq_residual_cov = get_sigma_sq_infty(observations)
   mse_kernel = np.mean((var_estimates - sigma_sq_infty)**2)
+  print('sq residual cov: {}'.format(sq_residual_cov))
   print('mse: {}'.format(mse_kernel))
 
 
@@ -124,13 +149,16 @@ def constant(x):
   return 1
 
 
-def var_estimates(bandwidths=(5, 15, 25), betas=(0.1,), grid_size=100):
+def var_estimates(bandwidths=(5,), betas=(0.1,), grid_size=100, n_rep=1000, pct_cores=0.25):
   for beta in betas:
     print('beta: {}'.format(beta))
-    _, root_cov, pairwise_distances = get_exponential_gaussian_covariance(beta1=beta, beta2=beta, grid_size=grid_size)
+    cov, root_cov, pairwise_distances = get_exponential_gaussian_covariance(beta1=beta, beta2=beta, grid_size=grid_size)
+    closed_form_sq_resid_sum = 2*(cov[0, :]**2).sum()
+    print('closed form sq resid sum: {}'.format(closed_form_sq_resid_sum))
     for b in bandwidths:
       kernel = lambda k: bartlett(k, b)
-      get_var_estimate_mse(kernel=kernel, root_cov=root_cov, pairwise_distances=pairwise_distances, n_rep=1000)
+      get_var_estimate_mse(kernel=kernel, root_cov=root_cov, pairwise_distances=pairwise_distances, n_rep=n_rep,
+                           pct_cores=pct_cores)
 
   return
 
@@ -138,15 +166,12 @@ def var_estimates(bandwidths=(5, 15, 25), betas=(0.1,), grid_size=100):
 def var_sigma_infty_from_exp_kernel(beta1=0.1, beta2=0.1, grid_size=100):
   cov, _, _ = get_exponential_gaussian_covariance(beta1=beta1, beta2=beta2, grid_size=grid_size)
   sigma_sq_infty = cov[0, :].sum()
-  squared_residuals_sum = (cov[0, :]**2).sum()
+  squared_residuals_sum = 2*(cov[0, :]**2).sum()
   print('sigma sq infty: {} sq resid sum: {}'.format(sigma_sq_infty, squared_residuals_sum))
   return
 
 
 if __name__ == "__main__":
-  var_sigma_infty_from_exp_kernel(grid_size=100)
-  var_sigma_infty_from_exp_kernel(grid_size=1600)
-  var_sigma_infty_from_exp_kernel(grid_size=2500)
-
-
+  bandwidths = (5, 10, 20, 40)
+  var_estimates(bandwidths=bandwidths, grid_size=10000, n_rep=10000)
 
