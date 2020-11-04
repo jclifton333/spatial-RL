123c6407844e07a0a93114f77c52cb0be89cde39
Jesse Clifton <jesseclifton@Jesses-iMac-Pro.local>
2020-11-04 23:29:33 +0000

    (wip) GGCN multi

--- src/estimation/q_functions/embedding.py
+++ src/estimation/q_functions/embedding.py
@@ -24,6 +24,50 @@ else:
 import pdb
 
 
+class GGCN_multi(nn.Module):
+  """
+  Different input layer for every number of neighbors.
+  """
+  def __init__(self, nfeat, J, neighbor_subset_limit=2, samples_per_k=None, recursive=False):
+    # ToDo: different J for each neighbor_subset_size?
+
+    super(GGCN, self).__init__()
+    self.input_layers = []
+    self.neighbor_subset_limit = neighbor_subset_limit
+    for neighbor_subset_size in range(2, neighbor_subset_limit+1):
+      self.input_layers.append(nn.Linear(nfeat*neighbor_subset_size, J))
+    self.h1 = nn.Linear(J*neighbor_subset_limit, 1)
+    self.samples_per_k = samples_per_k
+    self.J = J
+
+  def h(self, E):
+    E = F.relu(E)
+    E = self.h1(E)
+    return E
+
+  def forward(self, X_, adjacency_lst):
+    L = X_.shape[0]
+    final_ = torch.tensor([])
+    X_ = torch.tensor(X_).float()
+    for l in range(L):
+      neighbors_l = adjacency_lst[l] + [l]
+      N_l = len(neighbors_l)
+      full_embedding = torch.tensor([])
+      for neighbor_subset_size in range(2, np.min((N_l, self.neighbor_subset_limit))):
+        permutations_k = list(permutations(neighbors_l, int(neighbor_subset_size)))
+        if self.samples_per_k is not None:
+          permutations_k_ixs = np.random.choice(len(permutations_k), size=self.samples_per_k, replace=False)
+          permutations_k = [permutations_k[ix] for ix in permutations_k_ixs]
+        embedding_k = torch.zeros(self.J)
+        for permutation in permutations_k:
+          x_permutation = X_[permutation, :]
+          x_permutation = np.hstack(x_permutation)
+          embedding_permutation = self.input_layers[neighbor_subset_size-1](x_permutation)
+          embedding_k += embedding_permutation / len(permutations_k)
+        full_embedding = torch.cat(full_embedding, embedding_k)
+    E = self.h(full_embedding)
+    y = F.sigmoid(E)
+    return y
 
 
 class GGCN(nn.Module):
@@ -33,19 +77,22 @@ class GGCN(nn.Module):
   def __init__(self, nfeat, J, neighbor_subset_limit=2, samples_per_k=None, recursive=False):
     super(GGCN, self).__init__()
     if neighbor_subset_limit > 1 and recursive:
-      self.g1 = nn.Linear(2*J, 100)
-      self.g2 = nn.Linear(100, J)
-    self.h1 = nn.Linear(nfeat, 100)
-    self.h2 = nn.Linear(100, J)
-    self.final = nn.Linear(J + nfeat*(neighbor_subset_limit > 1), 1)
+      self.g1 = nn.Linear(2*J, 10)
+      self.g2 = nn.Linear(10, J)
+    self.h1 = nn.Linear(nfeat, 1)
+    # self.h1 = nn.Linear(nfeat, 10)
+    # self.h2 = nn.Linear(10, J)
+    # self.final = nn.Linear(J, 1)
     self.neighbor_subset_limit = neighbor_subset_limit
     self.J = J
     self.samples_per_k = samples_per_k
     self.recursive = recursive
 
   def h(self, b):
+    # ToDo: using linear architecture for debugging
     b = self.h1(b)
-    b = self.h2(b)
+    # b = F.relu(b)
+    # b = self.h2(b)
     return b
 
   def g(self, bvec):
@@ -76,19 +123,18 @@ class GGCN(nn.Module):
         if self.samples_per_k is not None:
           permutations_k_ixs = np.random.choice(len(permutations_k), size=self.samples_per_k, replace=False)
           permutations_k = [permutations_k[ix] for ix in permutations_k_ixs]
-        result = torch.zeros(self.J)
+        # result = torch.zeros(self.J)
+        result = torch.zeros(1)
         for perm in permutations_k:
+          # ToDo: just takes the first element of the permutation, makes no sense
           x_l1 = torch.tensor(X_[perm[0], :])
           h_val = self.h(x_l1)
           result += h_val / len(permutations_k)
         return result
 
-      if N_l > 1:
-        x_l = X_[l, :]
-        E_l = torch.cat((x_l, fk(N_l)))
-      else:
-        E_l = fk(N_l)
-      final_l = self.final(E_l)
+      E_l = fk(N_l)
+      final_l = E_l
+      # final_l = self.final(E_l)
       final_ = torch.cat((final_, final_l))
 
     params = list(self.parameters())
@@ -293,7 +339,7 @@ if __name__ == "__main__":
   adjacency_list = [[j for j in range(grid_size) if adjacency_mat[i, j]] for i in range(grid_size)]
   neighbor_counts = adjacency_mat.sum(axis=1)
   n = 2
-  n_epoch = 20
+  n_epoch = 100
   X_list = np.array([np.random.normal(size=(grid_size, 2)) for _ in range(2)])
   y_probs_list = np.array([np.array([expit(np.sum(X[adjacency_list[l]])) for l in range(grid_size)]) for X in X_list])
   y_list = np.array([np.array([np.random.binomial(1, prob) for prob in y_probs]) for y_probs in y_probs_list])
@@ -303,26 +349,11 @@ if __name__ == "__main__":
   predictor(X_list[0])
 
   print('Fitting ggcn')
-  learn_ggcn(X_list, y_list, adjacency_list, n_epoch=n_epoch, nhid=100, batch_size=5, verbose=True,
+  learn_ggcn(X_list, y_list, adjacency_list, n_epoch=n_epoch, nhid=20, batch_size=5, verbose=True,
              neighbor_subset_limit=2, samples_per_k=5, recursive=False)
 
-  mean = np.mean(y_list)
-  baseline = np.max((mean, 1 - mean))
-  print(f'baseline\n{mean}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  oracle_mean = 0.
+  for yp, y in zip(y_probs_list, y_list):
+    y_hat = (yp > 0.5)
+    oracle_mean += (y_hat == y).mean() / len(y_list)
+  print(f'oracle: {oracle_mean}')
