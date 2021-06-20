97163f5e183546c4f4a56b2a1b5bc6571f8dcff4
Jesse Clifton <jesseclifton@Jesses-iMac-Pro.local>
2021-06-20 17:34:21 +0100

    (wip) normalize with two step or oracle

--- analysis/results/merge_and_analyze_csvs.py
+++ analysis/results/merge_and_analyze_csvs.py
@@ -11,6 +11,7 @@ def merge_may_data():
   df2 = pd.read_csv('210531.csv')
   df3 = pd.read_csv('210601,210602.csv')
   df4 = pd.read_csv('oracle-incorrect-contaminated.csv')
+  df5 = pd.read_csv('210613,210614,210615,210616,210617.csv')
 
   # Add cols for raw features
   df1['raw0'] = 0
@@ -21,6 +22,11 @@ def merge_may_data():
   df3['raw1'] = 1
   df4['raw0'] = 0
   df4['raw1'] = 0
+  df5['raw0'] = 0
+  df5['raw1'] = 0
+
+  df4 = df4[~(df4['epsilon'].isin([0.5, 1.0]))]
+  df5 = df5[df5['policy_name'] == 'two_step_true_probs']
 
   # Merge csvs
   df = pd.concat([df1, df2, df3, df4])
@@ -43,7 +49,7 @@ def merge_may_data():
     df.loc[df.policy_name.isin(['one_step_ggcn', 'two_step_ggcn', 'two_step']), 'raw0'].astype(str) + \
     df.loc[df.policy_name.isin(['one_step_ggcn', 'two_step_ggcn', 'two_step']), 'raw1'].astype(str)
 
-  df.to_csv('0605_merged.csv')
+  df.to_csv('0620_merged.csv')
 
   return
 
@@ -52,9 +58,9 @@ def barplots(df, normalize=False):
   # full_env_names = ['lattice0.0', 'lattice0.5', 'lattice1.0']
   # full_env_names = df.full_env_name.unique()
   full_env_names = [name for name in df.full_env_name.unique()
-                    if (name != 'Ebola' and '0.5' not in name and '1.0' not in name)]  # ToDo: don't have oracle for Ebola
+                    if (name != 'Ebola')]  # ToDo: don't have oracle for Ebola
   L_list = [100]
-  epsilon_list = [0.0]  # ToDo: currently only using epsilon=0.0 because oracles for epsilon > 0.0 are wrong
+  epsilon_list = [0.0, 0.5, 1.0]
   df_subset = df[(df['full_env_name'].isin(full_env_names)) & (df['L'].isin(L_list)) &
                  (df['epsilon'].isin(epsilon_list))]
 
@@ -65,10 +71,21 @@ def barplots(df, normalize=False):
     # Get oracle and random performances
     for L in L_list:
       for full_env_name in df_subset.full_env_name.unique():
-        oracle_performance = \
-          df_subset.loc[(df_subset['L'] == L)
-                    & (df_subset['full_env_name'] == full_env_name)
-                    & (df_subset['full_policy_name'] == 'oracle_policy_search'), 'mean'].iloc[0]
+        if 'oracle_policy_search' in df_subset.policy_name.unique():
+          oracle_ps_performance = \
+            df_subset.loc[(df_subset['L'] == L)
+                      & (df_subset['full_env_name'] == full_env_name)
+                      & (df_subset['full_policy_name'] == 'oracle_policy_search'), 'mean'].iloc[0]
+          oracle_two_step_performance = \
+            df_subset.loc[(df_subset['L'] == L)
+                          & (df_subset['full_env_name'] == full_env_name)
+                          & (df_subset['full_policy_name'] == 'two_step_true_probs'), 'mean'].iloc[0]
+          oracle_performance = np.min((oracle_ps_performance, oracle_two_step_performance))
+        else:
+          oracle_performance = \
+            df_subset.loc[(df_subset['L'] == L)
+                          & (df_subset['full_env_name'] == full_env_name)
+                          & (df_subset['full_policy_name'] == 'two_step_true_probs'), 'mean'].iloc[0]
         random_performance = \
           df_subset.loc[(df_subset['L'] == L)
                     & (df_subset['full_env_name'] == full_env_name)
@@ -92,6 +109,12 @@ def barplots(df, normalize=False):
 
 if __name__ == "__main__":
   merge_may_data()
-  df = pd.read_csv('0605_merged.csv')
-  barplots(df, normalize=True)
+  df = pd.read_csv('0620_merged.csv') # barplots(df, normalize=True)
+  # df4 = pd.read_csv('oracle-incorrect-contaminated.csv')
 
+  # # Add cols for raw features
+  # df4['raw0'] = 0
+  # df4['raw1'] = 0
+
+  # df4 = df4[~(df4['epsilon'].isin([0.5, 1.0]))]
+  # print(df4)
