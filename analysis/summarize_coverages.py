098e6f0feab0978c5deb03a6e1af9afa6df5af08
Jesse Clifton <jesseclifton@Jesses-iMac-Pro.local>
2021-08-16 22:44:32 +0100

    beta to sigma

--- analysis/summarize_coverages.py
+++ analysis/summarize_coverages.py
@@ -12,7 +12,7 @@ from matplotlib.lines import Line2D
 
 def plot_coverages(df):
   df['L'] = df['L'].astype('category')
-  g = sns.relplot(data=df, x='bandwidth', y='coverages', hue='L', row='beta', col='backup', kind='line',
+  g = sns.relplot(data=df, x='bandwidth', y='coverages', hue='L', row='sigma_sq', col='backup', kind='line',
                   palette=['red', 'blue', 'orange'], legend=False)
   g.map(plt.axhline, y=0.95, c='black')
   custom_lines = [Line2D([0], [0], color='black', lw=1),
@@ -26,12 +26,13 @@ def plot_coverages(df):
     line_labels.append(f'L={L_}')
   # custom_lines = [Line2D([0], [0], color='black', lw=1)]
   plt.legend(custom_lines, line_labels, loc='lower right')
+  plt.savefig('coverages.png')
   plt.show()
   return
 
 
 def summarize_coverages_at_date(date_strs, save=False):
-  results = {'L': [], 'beta': [], 'bandwidth': [], 'coverages': [], 'backup': []}
+  results = {'L': [], 'sigma_sq': [], 'bandwidth': [], 'coverages': [], 'backup': []}
   any_found = False
   date_str_list = date_strs.split(',')
   for fname in os.listdir('coverages'):
@@ -48,12 +49,12 @@ def summarize_coverages_at_date(date_strs, save=False):
         # ToDo: throwing away data for grids bigger than 4000!
         if bandwidth_results['grid_size'] < 4000:
           results['L'].append(bandwidth_results['grid_size'])
-          results['beta'].append(bandwidth_results['beta'])
+          results['sigma_sq'].append(1 / bandwidth_results['beta'])
           results['bandwidth'].append(bandwidth)
           results['coverages'].append(bandwidth_results['coverage'])
           results['backup'].append(bandwidth_results['backup'])
     df = pd.DataFrame.from_dict(results)
-    df.sort_values(by=['backup', 'beta', 'L', 'bandwidth'], inplace=True)
+    df.sort_values(by=['backup', 'sigma_sq', 'L', 'bandwidth'], inplace=True)
     print(df)
     if save:
       df.to_csv(f'coverages/{date_strs}.csv')
@@ -67,7 +68,8 @@ if __name__ == "__main__":
   # parser = argparse.ArgumentParser()
   # parser.add_argument('--date', type=str)
   # args = parser.parse_args()
-  # summarize_coverages_at_date(args.date)
+  date = '210713,210714,210715,210716,210717.csv'
+  summarize_coverages_at_date(date)
   df = pd.read_csv('coverages/210713,210714,210715,210716,210717.csv')
   plot_coverages(df)
 
